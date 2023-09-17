b8142a0233f394121dcb6e2e9ea995078105c856
BLK LUV [org] <info@blkluv.org>
2023-09-16 22:27:09 -0400

    Update MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,21 +1,21 @@
 function MainPage() {
   return (
     <div className="jumbotron f-16 mt-3">
-      <h1 className="display-3">🅰️ TTV Rechat 🔄💬</h1>
+      <h1 className="display-3">🅰️ TTV Rechat</h1>
       <div className="col-12 border border-primary rounded-pill shadow py-2 mt-3">
         https://www.twitch<strong>gg</strong>.tv/videos/12345678
       </div>
       <hr className="my-4"></hr>
       <p>
         Imagine you're watching a video of your favorite streamer on Twitch, and you want to see what people were saying in the chat 💬 while the stream was happening. 
-
-To do that, you need to change the web link (the address at the top of your internet browser). You want to add "<strong>gg</strong>" after the word "twitch" in the link. 
-
-So the link would look like this: "www.twitchgg.com/streamername."
-
-By adding "<strong>gg</strong>" like that, you might be able to replay the chat and see what everyone was talking about during the stream. It's like going back in time to read the chat messages again! 🔄
-
-Powered by
+      </br>
+      To do that, you need to change the web link (the address at the top of your internet browser). You want to add "<strong>gg</strong>" after the word "twitch" in the link. 
+      </br>
+      So the link would look like this: "www.twitchgg.com/streamername."
+      </br>
+      By adding "<strong>gg</strong>" like that, you might be able to replay the chat and see what everyone was talking about during the stream. It's like going back in time to read the chat messages again! 🔄
+      </br>
+      Powered by
       </p>
       
       <a
@@ -27,6 +27,7 @@ Powered by
       >
         🅰️rvrtise
       </a>
+          <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="arvrtiser" data-color="#5F7FFF" data-emoji="❤️"  data-font="Arial" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
     </div>
   );
 }
