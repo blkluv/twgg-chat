30ecbd7eeffaecb1133d2a5f7ef0d7f30ea5d051
BLK LUV [org] <info@blkluv.org>
2023-09-16 22:50:27 -0400

    Update MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -5,19 +5,18 @@ function MainPage() {
       <div className="col-12 border border-primary rounded-pill shadow py-2 mt-3">
         https://www.twitch<strong>gg</strong>.tv/videos/12345678
       </div>
-      <hr className="my-4"></hr>
+      <hr className="my-4" />
       <p>
-        Imagine you're watching a video of your favorite streamer on Twitch, and you want to see what people were saying in the chat 💬 while the stream was happening. 
-      </br>
-      To do that, you need to change the web link (the address at the top of your internet browser). You want to add "<strong>gg</strong>" after the word "twitch" in the link. 
-      </br>
-      So the link would look like this: "www.twitchgg.com/streamername."
-      </br>
-      By adding "<strong>gg</strong>" like that, you might be able to replay the chat and see what everyone was talking about during the stream. It's like going back in time to read the chat messages again! 🔄
-      </br>
-      Powered by
+        Imagine you're watching a video of your favorite streamer on Twitch, and you want to see what people were saying in the chat 💬 while the stream was happening.
+        <br />
+        To do that, you need to change the web link (the address at the top of your internet browser). You want to add "<strong>gg</strong>" after the word "twitch" in the link.
+        <br />
+        So the link would look like this: "www.twitchgg.com/streamername."
+        <br />
+        By adding "<strong>gg</strong>" like that, you might be able to replay the chat and see what everyone was talking about during the stream. It's like going back in time to read the chat messages again! 🔄
+        <br />
       </p>
-      
+
       <a
         className="btn btn-primary btn-lg"
         href="https://arvrtise.com"
@@ -27,7 +26,19 @@ function MainPage() {
       >
         🅰️rvrtise
       </a>
-          <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="arvrtiser" data-color="#5F7FFF" data-emoji="❤️"  data-font="Arial" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
+      <script
+        type="text/javascript"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+        data-name="bmc-button"
+        data-slug="arvrtiser"
+        data-color="#5F7FFF"
+        data-emoji="❤️"
+        data-font="Arial"
+        data-text="Buy me a coffee"
+        data-outline-color="#000000"
+        data-font-color="#ffffff"
+        data-coffee-color="#FFDD00"
+      ></script>
     </div>
   );
 }
