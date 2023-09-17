8c693f2d630af544bde718117a05c7003144d2ce
BLK LUV [org] <info@blkluv.org>
2023-09-16 21:59:31 -0400

    Update MainPage.tsx

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,27 +1,31 @@
 function MainPage() {
   return (
     <div className="jumbotron f-16 mt-3">
-      <h1 className="display-3">TTV Rechat</h1>
+      <h1 className="display-3">🅰️ TTV Rechat🔄💬</h1>
       <div className="col-12 border border-primary rounded-pill shadow py-2 mt-3">
         https://www.twitch<strong>gg</strong>.tv/videos/12345678
       </div>
       <hr className="my-4"></hr>
       <p>
-        Insert "<strong>gg</strong>" after the word "twitch" in the link to
-        replay chats by streamers from Twitch streams.
+        Imagine you're watching a video of your favorite streamer on Twitch, and you want to see what people were saying in the chat 💬 while the stream was happening. 
+
+To do that, you need to change the web link (the address at the top of your internet browser). You want to add "<strong>gg</strong>" after the word "twitch" in the link. 
+
+So the link would look like this: "www.twitchgg.com/streamername."
+
+By adding "<strong>gg</strong>" like that, you might be able to replay the chat and see what everyone was talking about during the stream. It's like going back in time to read the chat messages again! 🔄
+
+Powered by
       </p>
-      <p lang="ko">
-        트위치 다시보기 링크의 "twitch" 뒤에 "<strong>gg</strong>"를 삽입하면
-        스트리머의 채팅을 모아볼 수 있습니다.
-      </p>
+      
       <a
         className="btn btn-primary btn-lg"
-        href="https://twgg.notion.site/Rechat-63ee83dd92384963a162895ac2553f70"
+        href="https://arvrtise.com"
         target="_blank"
         rel="noopener noreferrer"
         role="button"
       >
-        Info
+        🅰️rvrtise
       </a>
     </div>
   );
